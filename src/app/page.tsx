3675db97905c81762676747bd592d1f68c305b9f
fabry-js <vivaldifabrizio10@gmail.com>
2023-10-02 13:23:53 +0200

    feat: :sparkles: enhance index page ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="">
-      <Link href={"/profiles"}>Show profiles</Link>
+    <div className="flex h-screen items-center justify-center">
+      <Link href={"/profiles"}>
+        <button className="rounded-2xl bg-gray-900 px-6 py-4 transition-all duration-300 hover:bg-gray-800 active:scale-95 active:bg-gray-700">
+          Show profiles
+        </button>
+      </Link>
     </div>
   );
 }
