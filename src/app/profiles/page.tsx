cd230cab45772ba4c8d6316bf9c3e9d0659e2d17
fabry-js <vivaldifabrizio10@gmail.com>
2023-10-02 13:24:05 +0200

    feat: :sparkles: enhance profiles index ui

--- src/app/profiles/page.tsx
+++ src/app/profiles/page.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Profile() {
   return (
-    <div className="">
-      <p>Select a profile to get started</p>
+    <div className="mt-6 flex items-center justify-center">
+      <p className="opacity-70">Select a profile to get started</p>
     </div>
   );
 }
