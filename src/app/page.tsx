e85e9546271f1e947b0fa26a1f77c17c78fd7404
fabry-js <vivaldifabrizio10@gmail.com>
2023-10-02 12:12:09 +0200

    feat: :sparkles: update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,9 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center gap-1 p-4">
-      <h1 className="text-3xl font-bold">X Mock API with caching</h1>
-      <div className="border-b border-gray-700 text-center text-sm font-medium text-gray-400">
-        <ul className="-mb-px flex flex-wrap">
-          <li className="mr-2">
-            <a
-              href="#"
-              className="inline-block rounded-t-lg border-b-2 border-transparent p-4 hover:border-gray-300 hover:text-gray-300"
-            >
-              @nabla_hq
-            </a>
-          </li>
-          <li className="mr-2">
-            <a
-              href="#"
-              className="active inline-block rounded-t-lg border-b-2 border-blue-500 p-4 text-blue-500"
-              aria-current="page"
-            >
-              @osmosiszone
-            </a>
-          </li>
-          <li className="mr-2">
-            <a
-              href="#"
-              className="inline-block rounded-t-lg border-b-2 border-transparent p-4 hover:border-gray-300 hover:text-gray-300"
-            >
-              @GenshinImpact
-            </a>
-          </li>
-        </ul>
-      </div>
+    <div className="">
+      <Link href={"/profiles"}>Show profiles</Link>
     </div>
   );
 }
