39ccfafde0738a00c248c4d774a83656f58f0d53
fabry-js <vivaldifabrizio10@gmail.com>
2023-10-02 12:11:56 +0200

    feat: :sparkles: add navigation component

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { useSelectedLayoutSegment } from "next/navigation";
+
+function Navigation({ routes }: { routes: string[] }) {
+  const pathname = useSelectedLayoutSegment();
+  return (
+    <ul className="-mb-px flex flex-wrap">
+      {routes.map((route) => (
+        <li key={route} className="mr-2">
+          <Link
+            href={`/profiles/${route}`}
+            className={`${
+              route === pathname ? "activelink" : "unactivelink"
+            } hover:activelink inline-block rounded-t-lg border-b-2 border-transparent p-4`}
+          >
+            @{route}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Navigation;
